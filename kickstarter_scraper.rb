1e722605a31d222f7511d6d16a8bdfb7cf7d64c3
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-23 22:28:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def create_project_hash
   kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
-  
+
   projects = {}
 
   kickstarter.css("li.project.grid_4").each{ |project|
