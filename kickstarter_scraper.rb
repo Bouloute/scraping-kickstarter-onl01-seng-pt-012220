e4ea1dc5f44ba31af8ac496c2b2236214f2d0849
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-23 22:30:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each{ |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
+    projects[title] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
